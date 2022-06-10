6d4c0af022f850cbea29597753218b88e38682ec
diavol0 <morto_diavolo@bk.ru>
2022-06-11 00:37:15 +0300

    add for while do-while to help

--- p311.cpp
+++ p311.cpp
@@ -7,7 +7,14 @@ public:
 	int passengers ;
 	int fuelcap    ;
 	int mpg        ;	
+
+	int range()    ;
 };
+
+int Vehicle::range()
+{
+	return mpg * fuelcap ;
+}
 
 int main ()
 {
@@ -16,16 +23,16 @@ int main ()
 	int     range1   ;
 	int     range2   ;
 	
-	minivan.passengers  = 7  ;
-	minivan.fuelcap     = 16 ;
-	minivan.mpg         = 21 ;
+	minivan.passengers  = 7    ;
+	minivan.fuelcap     = 16   ;
+	minivan.mpg         = 21   ;
 
-	sportcar.passengers = 2  ;
-        sportcar.fuelcap    = 14 ;
-        sportcar.mpg        = 12 ;
+	sportcar.passengers = 2    ;
+        sportcar.fuelcap    = 14   ;
+        sportcar.mpg        = 12   ;
 
-	range1 = minivan.fuelcap  * minivan.mpg  ;
-        range2 = sportcar.fuelcap * sportcar.mpg ;
+	range1 = minivan.range()   ;
+        range2 = sportcar.range()  ;
 
         cout << " Фургон может проехать расстояние: "            << range1 << endl;
 	cout << " Спортивная машина может проехать расстояние: " << range2 << endl;
